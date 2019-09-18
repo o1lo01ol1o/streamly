40012134fd28365afb8443daaacb35e24b9ae891
Pranay Sashank <pranaysashank@gmail.com>
2019-09-19 00:57:17 +0530

    Control number of runs from cmd line with `-fplugin-opt Plugin:n`.

--- plugin/Plugin.hs
+++ plugin/Plugin.hs
@@ -9,4 +9,5 @@ plugin =
     defaultPlugin {installCoreToDos = install}
 
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
+install (n:_) todo = return $ replicate (read n) todo >>= id
 install _ todo = return $ todo ++ todo ++ todo
